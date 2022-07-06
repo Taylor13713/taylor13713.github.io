3e7e7400ef46f539eb65fffa47789ede944ebaa4
Taylor <tli13713@gmail.com>
2022-07-05 20:09:50 -0400

    project transfer complete

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ publish(
   'build', // path to public directory
   {
     branch: 'gh-pages',
-    repo: 'https://github.com/Taylor13713/taylor-portfolio.git', // Update to point to your repository
+    repo: 'https://github.com/Taylor13713/taylor13713.github.io.git', // Update to point to your repository
     user: {
       name: 'Taylor Li', // update to use your name
       email: 'tli13713@gmail.com' // Update to use your email
